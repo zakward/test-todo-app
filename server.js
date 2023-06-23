a1e7609d9acabae303d9fe6e1ff9157906644cd1
zakward85 <zakward85@gmail.com>
2023-06-23 13:22:00 -0500

    testdeploy

--- server.js
+++ server.js
@@ -5,11 +5,18 @@ const app = express();
 require("dotenv").config();
 const port = process.env.PORT;
 const DB_URL = process.env.DB_URL;
+const path = require("path"); //required for deployment
 
 app.use(express.json());
 app.use(morgan("dev"));
 
 mongoose.set("strictQuery", false);
+
+app.use(express.static(path.join(__dirname, "client", "dist"))); // middleware for deployment  //dist for vite and build for cra
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "client", "dist", "index.html")); // middleware for deployment // dist for vite
+});
 
 mongoose.connect(
   DB_URL,
