21b4871ca7e5b2ba58ce071f2ee6b8d3f8ad36d8
zakward85 <zakward85@gmail.com>
2023-09-03 13:53:06 -0500

    app changes to ted tracker

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,128 +1,27 @@
-import { useState } from "react";
-import axios from "axios";
+import React, { useContext } from "react";
+import Login from "./Login";
+import { Routes, Route, Navigate, Link } from "react-router-dom";
+import Ted from "./Ted";
+import { UserContext } from "./context/userContext";
+import BudForm from "./BudForm";
 
-function App() {
-  //state for getting all todos
-  const [initTodos, setInitTodos] = useState([]);
-  const [todos, setTodos] = useState([]);
-
-  //boolean to toggle the add todo form
-  const [formToggle, setFormToggle] = useState(false);
-
-  //state for user inputs
-  const [initInputs, setInitInputs] = useState({
-    title: "",
-    description: "",
-  });
-  const [inputs, setInputs] = useState(initInputs);
-
-  // variable for rendering all todos
-  const todoList = todos.map((todo) => {
-    return (
-      <>
-        <div key={todo._id} id="todo-card">
-          <h2>Title: {todo.title}</h2>
-          <h3>Description: {todo.description}</h3>
-          {/* <h4>Completed: {todo.completed ? "yes" : "no"}</h4> */}
-          <button className="delete-btn" onClick={() => deleteTodo(todo._id)}>
-            Delete
-          </button>
-        </div>
-      </>
-    );
-  });
-
-  // get request for all todos
-  function getTodos() {
-    axios
-      .get("/api/todo")
-      .then((res) => setTodos(res.data))
-      .catch((err) => console.log(err));
-  }
-
-  //clears all todos from the page
-  function clearTodos() {
-    setTodos(initTodos);
-  }
-
-  //handlechange function to update state of user inputs
-  function handleInputsChange(e) {
-    const { name, value } = e.target;
-    setInputs((prevInputs) => {
-      return {
-        ...prevInputs,
-        [name]: value,
-      };
-    });
-  }
-
-  //post request to add a new todo
-  function addTodo() {
-    axios
-      .post(`/api/todo/`, inputs)
-      .then((res) =>
-        setTodos((prevTodos) => {
-          return [res.data, ...prevTodos];
-        })
-      )
-      .catch((err) => console.log(err));
-    setInputs(initInputs);
-    setFormToggle(!formToggle);
-  }
-
-  //deleteing an individual todo from the db
-  function deleteTodo(iD) {
-    axios
-      .delete(`/api/todo/${iD}`)
-      .then((res) =>
-        setTodos((prevTodos) => prevTodos.filter((todo) => todo._id !== iD))
-      )
-      .catch((err) => console.log(err));
-  }
+export default function App() {
+  const { token, logout } = useContext(UserContext);
 
   return (
     <>
-      <div id="main-container">
-        <h1 id="app-title">This is a test App using Vite </h1>
-        <span id="button-span">
-          <button onClick={getTodos}>Get All Todos</button>
-          <button onClick={clearTodos}>Clear Todo List</button>
-          <button onClick={() => setFormToggle(!formToggle)}>
-            Make New Todo
-          </button>
-        </span>
-
-        {formToggle && (
-          <form id="addForm" onSubmit={addTodo}>
-            <h1>Add Todo Form</h1>
-            <input
-              placeholder="add title"
-              name="title"
-              value={inputs.title}
-              onChange={handleInputsChange}
-            />
-            <input
-              placeholder="add description"
-              name="description"
-              value={inputs.description}
-              onChange={handleInputsChange}
-            />
-            {/* <input
-            placeholder="completed"
-            name="completed"
-            value={inputs.completed}
-            onChange={handleInputsChange}
-          /> */}
-            <button>SUBMIT</button>
-          </form>
-        )}
-      </div>
-      <div id="todos-container">
-        <h1>Todos from the API</h1>
-        {todoList}
-      </div>
+      <header>
+        <h3>T3D TRACK3R</h3>
+        <Link to="/budForm">
+          <button>Add New Bud</button>
+        </Link>
+        <button onClick={logout}>LogOut</button>
+      </header>
+      <Routes>
+        <Route path="/ted" element={token ? <Ted /> : <Navigate to="/" />} />
+        <Route path="/" element={token ? <Navigate to="/ted" /> : <Login />} />
+        <Route path="/budform" element={token ? <BudForm /> : <Login />} />
+      </Routes>
     </>
   );
 }
-
-export default App;
--- server.js
+++ server.js
@@ -1,25 +1,30 @@
 const express = require("express");
-const morgan = require("morgan");
 const mongoose = require("mongoose");
 const app = express();
+const morgan = require("morgan");
 require("dotenv").config();
-const port = process.env.PORT;
-const DB_URL = process.env.DB_URL;
+const { expressjwt } = require("express-jwt");
+
 const path = require("path"); //required for deployment
 
 app.use(express.json());
 app.use(morgan("dev"));
-
 mongoose.set("strictQuery", false);
 
 app.use(express.static(path.join(__dirname, "client", "dist"))); // middleware for deployment  //dist for vite and build for cra
-
-mongoose.connect(DB_URL, (err) => {
-  if (err) throw err;
-  console.log("Connected to the Database!");
+mongoose.connect(process.env.MONGO_URI, (err) => {
+  if (err) {
+    throw err;
+  }
+  console.log("Connected to MongoDB");
 });
-
-app.use("/api/todo", require("./routes/todoRouter"));
+app.use(
+  "/api/main",
+  expressjwt({ secret: process.env.SECRET, algorithms: ["HS256"] })
+);
+app.use("/api/auth", require("./routes/authRouter"));
+app.use("/api/main/ted", require("./routes/tedRouter"));
+app.use("/api/main/comments", require("./routes/commentsRouter"));
 
 app.use((err, req, res, next) => {
   console.log(err);
@@ -28,10 +33,9 @@ app.use((err, req, res, next) => {
   }
   return res.send({ errMsg: err.message });
 });
-
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "client", "dist", "index.html")); // middleware for deployment // dist for vite
-}),
-  app.listen(port, () => {
-    console.log(`Server is listening on port ${port}`);
-  });
+});
+app.listen(8800, () => {
+  console.log("listening on port 8800");
+});
